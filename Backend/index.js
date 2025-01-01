cd06b2560b6b22e75a55f14f9fa12fed1d7779fc
Arpit Agrawal <agrawalarpit456@gmail.com>
2025-01-01 23:01:22 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,10 +10,6 @@ app.get('/' , (req , res) => {
     res.send("<h1>MERN Chat App</h1>")
 })
 
-app.get('/hemlo' , (req , res) => {
-    res.send("<h1>Testing The Express Knowledge</h1>")
-})
-
 app.listen(port , () => {
     console.log(`Server is listening on the Port : ${port}`) ;
 }) ;
