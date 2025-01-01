fca77a1815ab97eecafc71168aea19f9c15da5e0
Arpit Agrawal <agrawalarpit456@gmail.com>
2025-01-01 18:17:25 +0530

    Update index.js

---
+++ Backend/index.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv' ;
+dotenv.config() ;
+
+import express from 'express' ;
+const app = express() ;
+
+const port = process.env.PORT || 8000 ;
+
+app.get('/' , (req , res) => {
+    res.send("<h1>MERN Chat App</h1>")
+})
+
+app.listen(port , () => {
+    console.log(`Server is listening on the Port : ${port}`) ;
+}) ;
